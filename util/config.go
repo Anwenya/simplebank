71b38805336b4566d545ede77f361120c407633e
wuliuqi <1737682009@qq.com>
2023-09-26 15:48:23 +0000

    解析配置文件

---
+++ util/config.go
@@ -0,0 +1,27 @@
+package util
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	// 配置文件名称
+	viper.SetConfigName("app")
+	// 配置文件格式
+	viper.SetConfigType("env")
+	// 环境变量中的值会覆盖配置文件中的同名值
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
