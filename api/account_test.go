0062d1118a4550e9b113840ab68436cb380847a7
wuliuqi <1737682009@qq.com>
2023-10-06 09:02:51 +0000

    适配NewServer变动

--- api/account_test.go
+++ api/account_test.go
@@ -101,7 +101,7 @@ func TestGetAccountAPI(t *testing.T) {
 			tc.buildStubs(store)
 
 			// 启动服务并模拟发起请求
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/accounts/%d", tc.accountId)
