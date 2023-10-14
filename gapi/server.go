bd63400bba6b1b48ac8bbad35e32ad050ae79454
wuliuqi <1737682009@qq.com>
2023-10-14 10:30:24 +0000

    新增gRPC服务

---
+++ gapi/server.go
@@ -0,0 +1,34 @@
+package gapi
+
+import (
+	"fmt"
+
+	db "com.wlq/simplebank/db/sqlc"
+	"com.wlq/simplebank/pb"
+	"com.wlq/simplebank/token"
+	"com.wlq/simplebank/util"
+)
+
+// gRPC服务
+type Server struct {
+	pb.UnimplementedSimpleBankServer
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+}
+
+// 创建gRPC服务
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker:%w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
+	return server, nil
+}
