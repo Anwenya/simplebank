c85cca064876ef4e1d5918a22edacba3f74387a5
wuliuqi <1737682009@qq.com>
2023-10-18 14:32:24 +0000

    为接口添加鉴权

--- gapi/rpc_update_user.go
+++ gapi/rpc_update_user.go
@@ -15,9 +15,19 @@ import (
 )
 
 func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+	// 校验令牌是否合法
+	authPayload, err := server.authorizeUser(ctx)
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
+
 	violations := validateUpdateUserRequest(req)
 	if violations != nil {
 		return nil, invalidArgumentError(violations)
+	}
+
+	if authPayload.Username != req.GetUsername() {
+		return nil, status.Errorf(codes.PermissionDenied, "cannot update other user's info")
 	}
 
 	arg := db.UpdateUserParams{
