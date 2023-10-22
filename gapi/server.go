855cfa117bc8e474882fa71a908c7dc1b6b9f331
wuliuqi <1737682009@qq.com>
2023-10-22 16:19:45 +0800

    组合TaskDistributor

--- gapi/server.go
+++ gapi/server.go
@@ -7,27 +7,34 @@ import (
 	"com.wlq/simplebank/pb"
 	"com.wlq/simplebank/token"
 	"com.wlq/simplebank/util"
+	"com.wlq/simplebank/worker"
 )
 
 // gRPC服务
 type Server struct {
 	pb.UnimplementedSimpleBankServer
-	config     util.Config
-	store      db.Store
-	tokenMaker token.Maker
+	config          util.Config
+	store           db.Store
+	tokenMaker      token.Maker
+	taskDistributor worker.TaskDistributor
 }
 
 // 创建gRPC服务
-func NewServer(config util.Config, store db.Store) (*Server, error) {
+func NewServer(
+	config util.Config,
+	store db.Store,
+	taskDistributor worker.TaskDistributor,
+) (*Server, error) {
 	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
 	if err != nil {
 		return nil, fmt.Errorf("cannot create token maker:%w", err)
 	}
 
 	server := &Server{
-		config:     config,
-		store:      store,
-		tokenMaker: tokenMaker,
+		config:          config,
+		store:           store,
+		tokenMaker:      tokenMaker,
+		taskDistributor: taskDistributor,
 	}
 
 	return server, nil
