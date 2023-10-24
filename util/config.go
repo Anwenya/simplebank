72a6ab3ea7dfbf86cd9429cb652b6820ef6a6398
wuliuqi <1737682009@qq.com>
2023-10-24 22:53:47 +0800

    新增邮箱相关依赖

--- util/config.go
+++ util/config.go
@@ -7,16 +7,22 @@ import (
 )
 
 type Config struct {
-	ENVIRONMENT          string        `mapstructure:"ENVIRONMENT"`
-	DBDriver             string        `mapstructure:"DB_DRIVER"`
-	DBSource             string        `mapstructure:"DB_SOURCE"`
-	MigrationURL         string        `mapstructure:"MIGRATION_URL"`
-	RedisAddress         string        `mapstructure:"REDIS_ADDRESS"`
-	HTTPServerAddress    string        `mapstructure:"HTTP_SERVER_ADDRESS"`
-	GRPCServerAddress    string        `mapstructure:"GRPC_SERVER_ADDRESS"`
-	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
-	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
-	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+	ENVIRONMENT                string        `mapstructure:"ENVIRONMENT"`
+	DBDriver                   string        `mapstructure:"DB_DRIVER"`
+	DBSource                   string        `mapstructure:"DB_SOURCE"`
+	MigrationURL               string        `mapstructure:"MIGRATION_URL"`
+	RedisAddress               string        `mapstructure:"REDIS_ADDRESS"`
+	HTTPServerAddress          string        `mapstructure:"HTTP_SERVER_ADDRESS"`
+	GRPCServerAddress          string        `mapstructure:"GRPC_SERVER_ADDRESS"`
+	TokenSymmetricKey          string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration        time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	RefreshTokenDuration       time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+	EmailGmailSenderName       string        `mapstructure:"EMAIL_GMAIL_SENDER_NAME"`
+	EmailGmailSenderAddress    string        `mapstructure:"EMAIL_GMAIL_SENDER_ADDRESS"`
+	EmailGmailSenderPassword   string        `mapstructure:"EMAIL_GMAIL_SENDER_PASSWORD"`
+	EmailTencentSenderName     string        `mapstructure:"EMAIL_TENCENT_SENDER_NAME"`
+	EmailTencentSenderAddress  string        `mapstructure:"EMAIL_TENCENT_SENDER_ADDRESS"`
+	EmailTencentSenderPassword string        `mapstructure:"EMAIL_TENCENT_SENDER_PASSWORD"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
