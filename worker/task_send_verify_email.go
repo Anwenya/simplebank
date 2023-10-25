849295478a4f4bdfcedd7a5fb40ca8b542e371aa
wuliuqi <1737682009@qq.com>
2023-10-25 23:32:04 +0800

    发送邮件

--- worker/task_send_verify_email.go
+++ worker/task_send_verify_email.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 
+	db "com.wlq/simplebank/db/sqlc"
+	"com.wlq/simplebank/util"
 	"github.com/hibiken/asynq"
 	"github.com/rs/zerolog/log"
 )
@@ -48,6 +50,26 @@ func (processor *RedisTaskProcessor) ProcessTaskSendVerifyEmail(ctx context.Cont
 		// 	return fmt.Errorf("user doesn't exist: %w", asynq.SkipRetry)
 		// }
 		return fmt.Errorf("failed to get user: %w", err)
+	}
+
+	// 创建入库的邮件
+	verifyEmail, err := processor.store.CreateVerifyEmail(ctx, db.CreateVerifyEmailParams{
+		Username:   user.Username,
+		Email:      user.Email,
+		SecretCode: util.RandomString(32),
+	})
+
+	subject := "welcome to simple bank"
+	verifyUrl := fmt.Sprintf("http://simple-bank.org/v1/verify_email?email_id=%d&secret_code=%s",
+		verifyEmail.ID, verifyEmail.SecretCode)
+	content := fmt.Sprintf(`hello %s, <br/>
+	Thank you for registering with us!<br/>
+	Please <a href="%s">click here</a> to verify your email address.<br/>
+	`, user.FullName, verifyUrl)
+	to := []string{user.Email}
+	err = processor.mailer.SendEmail(subject, content, to, nil, nil, nil)
+	if err != nil {
+		return fmt.Errorf("failed to send verify email: %w", err)
 	}
 
 	log.Info().Str("type", task.Type()).Bytes("payload", task.Payload()).
